c654d2664c7012c0c4cdeb8cbea7d7cd2120b6b0
zardat <b20161@students.iitmandi.ac.in>
2022-11-07 00:15:49 +0530

    Create database.sql

---
+++ Database/database.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM interviewDB.interviewee
+LIMIT 0, 1000
+
+-- Date: 2022-11-06 23:35
+*/
+INSERT INTO `` (`Interviewee`,`StartTime`,`EndTime`) VALUES ('Arvind','2022-11-06 12:00:00','2022-11-06 13:59:00');
+INSERT INTO `` (`Interviewee`,`StartTime`,`EndTime`) VALUES (Ujjwal','2022-11-06 15:03:00','2022-11-06 17:03:00');
+INSERT INTO `` (`Interviewee`,`StartTime`,`EndTime`) VALUES ('Tom','2022-11-07 15:03:00','2022-11-07 17:03:00');
+INSERT INTO `` (`Interviewee`,`StartTime`,`EndTime`) VALUES ('Rocky','2022-11-07 16:04:00','2022-11-07 17:03:00');
